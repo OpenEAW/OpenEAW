38190e9071e18c4b40221d49d05c35d835a5faa3
Mike Lankamp <mike.lankamp@gmail.com>
2022-07-11 22:41:28 +0200

    feat: display empty window
    Although the window is empty, this sets up the game and rendering loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "version.hpp"
 
-#include <khepri/application/console_logger.hpp>
+#include <khepri/application/application.hpp>
 #include <khepri/log/log.hpp>
+#include <khepri/renderer/diligent/renderer.hpp>
 #include <khepri/version.hpp>
 #include <openglyph/version.hpp>
 
@@ -11,10 +12,34 @@ constexpr auto APPLICATION_NAME = "OpenEAW";
 constexpr khepri::log::Logger LOG("application");
 } // namespace
 
+class MainApplication : public khepri::application::Application
+{
+public:
+    MainApplication() : khepri::application::Application(APPLICATION_NAME) {}
+
+private:
+    void do_run(khepri::application::Window& window, const std::filesystem::path& working_path) override
+    {
+        khepri::renderer::diligent::Renderer renderer(
+            Diligent::Win32NativeWindow(window.native_handle()));
+        window.add_size_listener([&] { renderer.render_size(window.render_size()); });
+        renderer.render_size(window.render_size());
+
+        while (!window.should_close()) {
+            khepri::application::Window::poll_events();
+            renderer.clear(khepri::renderer::Renderer::clear_all);
+            renderer.present();
+        }
+    }
+};
+
 int main()
 {
-    khepri::application::ConsoleLogger console_logger;
+    MainApplication application;
+
     LOG.info("Running {} {} (OpenGlyph {}, Khepri {})", APPLICATION_NAME,
              to_string(openeaw::version()), to_string(openglyph::version()),
              to_string(khepri::version()));
+
+    application.run();
 }
